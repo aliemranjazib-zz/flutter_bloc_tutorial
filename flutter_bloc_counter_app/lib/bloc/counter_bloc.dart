cd055cbbd15c6fa11781a3c8930ed16e68c4056a
Muhammad Imran Manzoor <aliemranjazib@gmail.com>
2023-06-30 21:08:09 +0500

    flutter_bloc

---
+++ flutter_bloc_counter_app/lib/bloc/counter_bloc.dart
@@ -0,0 +1,11 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'counter_event.dart';
+part 'counter_state.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(CounterInitial()) {
+    on<Increament>((event, emit) => emit(state + 1));
+  }
+}
